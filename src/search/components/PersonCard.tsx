260808e47318994e14360c95e4a371dc360bfb74
Ales Drobysh <adrobysh@evolution.com>
2021-03-13 11:53:15 +0100

    Implement person card

--- src/search/components/PersonCard.tsx
+++ src/search/components/PersonCard.tsx
@@ -1,21 +1,54 @@
 import { useSelector } from 'react-redux';
-import { Card, makeStyles } from "@material-ui/core";
+import { Card, List, ListItem, ListItemText, makeStyles } from "@material-ui/core";
 
-import { getSelectedOption } from '../store';
+import { getSelectedOption, Person } from '../store';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: 300 - theme.spacing(2),
     wordBreak: 'break-word',
     padding: theme.spacing(),
+  },
+  label: {
+    color: theme.palette.text.secondary,
+    flexBasis: '50%',
+  },
+  text: {
+    flexBasis: '50%',
   }
 }));
+
+const personKey2label: Record<keyof Person, string> = {
+  name: 'Name',
+  height: 'Height',
+  mass: 'Mass',
+  hairColor: 'Hair Color',
+  skinColor: 'Skin Color',
+  eyeColor: 'Eye Color',
+  birthYear: 'Birth Year',
+  gender: 'Gender',
+}
 
 export const PersonCard = () => {
   const styles = useStyles();
   const person = useSelector(getSelectedOption);
 
+  if (!person) {
+    return null;
+  }
+
   return (
-    person && <Card className={styles.root}>{JSON.stringify(person)}</Card>
+    <Card className={styles.root}>
+      <List>
+        {
+          Object.keys(person).map((key) => (
+            <ListItem key={key}>
+              <ListItemText className={styles.label}>{personKey2label[key as keyof Person]}</ListItemText>
+              <ListItemText className={styles.text}>{person[key as keyof Person]}</ListItemText>
+            </ListItem>
+          ))
+        }
+      </List>
+    </Card>
   );
 }
